13529a8671be9ed3a1c41da68d000aa96c11a20b
ferrrrrrry <ferrovicila@gmail.com>
2026-01-29 15:11:12 +0300

    Add files via upload

---
+++ js-home-1/main.js
@@ -0,0 +1,28 @@
+
+// задача 1
+const FerstName = "Илья";
+
+const LastName = "Терехов";
+
+const isStudent = true;
+
+// Задача 2 
+
+const Age = 17;
+
+const CurrentYear = 2026;
+
+ const birthYear = CurrentYear - Age;
+
+ console.log(birthYear);
+
+ // Задача 4
+
+let a = '123';
+let b = +'456';
+let c = Number('789');
+let d = Boolean(0);
+let e = Boolean(' ');
+let result = a + b + c + d + e;
+
+console.log(result); // ответ: 123456789falsetrue
